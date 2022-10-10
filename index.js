e186d316484d4a54f4968698610222b4c3048fbf
Andres Herrera <andrescamiloherreracubaque1908@gmail.com>
2022-10-10 12:07:49 -0500

    Actividad Portafolio

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const path = require('path')
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
+app.get('/', (req, res) => {
+  //res.send('Pagina Principal')
+  console.log(__dirname)
+  res.render('index')
+})
+
+app.get('/educacion', (req, res) => {
+  res.render('educacion',{ 
+  titulo: "Seccion Educacion",
+  descripcion: "Aqui encontraras mi formacion academica"
+  })
+})
+
+app.get('/laboral', (req, res) => {
+  res.send('Pagina de experencia laboral')
+})
+
+app.use((req, res, next) =>{
+  res.status(404).sendfile(__dirname + '/public/404.html')
+})
+
+app.listen(port, () => {
+  console.log(`Accede al servidor haciendo click aqui http/localhost:${port}`)
+})
